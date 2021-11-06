0f6414787bfd2a7e9c1704be391e4bfd6e9e65e2
Rickard Bemm <Rickos99@users.noreply.github.com>
2021-11-06 15:50:17 +0100

    Added endpoint to test webhook

--- main.js
+++ main.js
@@ -9,7 +9,11 @@ const HOST = "0.0.0.0";
 // App
 const app = express();
 app.get("/", (req, res) => {
-    res.send("Hello World. This was automatically built and deployed by Jenkins.");
+    res.send("Hello World.");
+});
+
+app.get("/test", (req, res) => {
+    res.send("This was automatically built and deployed by Jenkins.");
 });
 
 app.listen(PORT, HOST);
