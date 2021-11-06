cc880ee747e5d85ce18cc66862ef61f869f834f4
Rickard Bemm <Rickos99@users.noreply.github.com>
2021-11-06 15:26:48 +0100

    Test webhook

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const HOST = "0.0.0.0";
 // App
 const app = express();
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.send("Hello World. This was automatically built and deployed by Jenkins.");
 });
 
 app.listen(PORT, HOST);
