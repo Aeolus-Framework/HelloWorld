a2286c5dfd2414d9990eb595cafcf93b4e0a0a27
Rickos99 <Rickos99@users.noreply.github.com>
2021-11-13 16:53:48 +0100

    Fixed failing test

--- test/sum.test.ts
+++ test/sum.test.ts
@@ -1,5 +1,5 @@
-import sum from "../src/sum";
+import { default as math } from "../src/math";
 
 test("adds 1 + 2 to equal 3", () => {
-    expect(sum(1, 2)).toBe(3);
+    expect(math.sum(1, 2)).toBe(3);
 });
